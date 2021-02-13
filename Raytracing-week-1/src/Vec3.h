60d623f6789a2611642ec55eb0125e42f8a46802
Krzysztof Bański <krzys0100@wp.pl>
2021-02-13 18:40:45 +0100

    Finished the book 1

--- Raytracing-week-1/src/Vec3.h
+++ Raytracing-week-1/src/Vec3.h
@@ -44,6 +44,12 @@ public:
         return points[0] * points[0] + points[1] * points[1] + points[2] * points[2];
     }
 
+    bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions.
+        const auto s = 1e-8;
+        return (fabs(points[0]) < s) && (fabs(points[1]) < s) && (fabs(points[2]) < s);
+    }
+
     inline static vec3 random() {
         return vec3(random_double(), random_double(), random_double());
     }
@@ -103,6 +109,17 @@ inline vec3 normalized(vec3 v) {
     return v / v.length();
 }
 
+vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2 * dot(v, n) * n;
+}
+
+vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
+}
+
 vec3 random_in_unit_sphere() {
     while (true) {
         auto p = vec3::random(-1, 1);
@@ -113,4 +130,12 @@ vec3 random_in_unit_sphere() {
 
 vec3 random_unit_vector() {
     return normalized(random_in_unit_sphere());
+}
+
+vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
 }
